eb067d72058c6d0941d2e787d89e2b9816ee469e
Samihat <sameerah11@hotmail.com>
2021-11-04 12:06:50 -0400

    removing modelRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const modelRoutes = require("./modelRoutes");
 
 router.use("/user", userRoutes);
-router.use("/models", modelRoutes);
 
 module.exports = router;
