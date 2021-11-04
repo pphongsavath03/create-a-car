9ee9deb91df5ed070084c7ee341e6c6a85eb48d1
Samihat <sameerah11@hotmail.com>
2021-11-04 12:35:56 -0400

    adding login authentication

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const session = require('express-session');
+
+const withAuth = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
