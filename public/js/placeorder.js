7aaa945179b43c7f87b21bba2e2ba0ea662bc596
Phon Phongsavath <pphongsavath03@gmail.com>
2021-11-06 13:55:32 -0400

    place order js init

--- public/js/placeorder.js
+++ public/js/placeorder.js
@@ -1,12 +1,12 @@
 const placeOrders = async(event) => {
     event.preventDefault();
 
-    const exColor = document.querySelector('#ex-color').value
-    const inColor = document.querySelector('#int-color').value
-    const wheels = document.querySelector('#wheels').value
+    const exColor = document.querySelector('input[name="color-choice"]:checked').value
+    const intColor = document.querySelector('input[name="color-choice"]:checked').value
+    const wheels = document.querySelector('input[name="wheels"]:checked').value
 
-    
+    console.log(exColor, intColor, wheels)
 }
 
 
-document.querySelector('#place-order').addEventListener('submit', signupFormHandler);
+document.querySelector('#place-order').addEventListener('submit', placeOrders);
