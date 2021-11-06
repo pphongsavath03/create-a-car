29a8c288874774d70230718b21dd9f2e3c194fe3
Phon Phongsavath <pphongsavath03@gmail.com>
2021-11-06 13:10:03 -0400

    adding order placement js

---
+++ public/js/placeorder.js
@@ -0,0 +1,12 @@
+const placeOrders = async(event) => {
+    event.preventDefault();
+
+    const exColor = document.querySelector('#ex-color').value
+    const inColor = document.querySelector('#int-color').value
+    const wheels = document.querySelector('#wheels').value
+
+    
+}
+
+
+document.querySelector('#place-order').addEventListener('submit', signupFormHandler);
