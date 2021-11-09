03832c0a06dedd8dc6ea9a2939ecfe877c3b292a
Phon Phongsavath <pphongsavath03@gmail.com>
2021-11-08 22:35:54 -0500

    fixed login authentication

--- models/User.js
+++ models/User.js
@@ -31,24 +31,20 @@ User.init(
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [6],
-      },
     },
   },
   {
     hooks: {
-      async beforeCreate(newUserData) {
+      beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
+        if (updatedUserData.password) { 
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        }
         return updatedUserData;
-      },
+      }
     },
     sequelize,
     timestamps: false,
