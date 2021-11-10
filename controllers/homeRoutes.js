76c6dc528eadfefeb4027e1c5d2653b7377d2809
Samihat <sameerah11@hotmail.com>
2021-11-09 19:23:43 -0500

    styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -19,15 +19,14 @@ router.get('/product/:id',withAuth,  async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id);
     const product = productData.get({ plain: true });
-    res.render('product', { ...product, loggedIn: req.session.loggedIn });
+    res.render('product', { ...product, loggedIn: req.session.loggedIn, title: 'login', layout: 'cart-layout' });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.get('/cart', withAuth, (req, res) => {
-
-  res.render('cart', { title: 'Login', layout: 'cart-layout' });
+  res.render('cart', { loggedIn: req.session.loggedIn, title: 'Login', layout: 'cart-layout' });
 });
 
 router.get('/login', (req, res) => {
